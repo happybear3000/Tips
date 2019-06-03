9aa36a18b0099370006002926468b06e9a9e1a55
Edward <happybear3000@126.com>
2019-06-03 10:01:57 +0800

    画五角星

---
+++ penta.py
@@ -0,0 +1,20 @@
+#五角星画法
+"""
+作者
+时间
+"""
+
+import turtle
+
+def main():
+ 
+    count = 1
+    while count <= 5:
+         turtle.forward(200)
+         turtle.right(144)
+         count = count + 1
+    
+    turtle.exitonclick()
+
+if __name__ == '__main__':
+    main()
