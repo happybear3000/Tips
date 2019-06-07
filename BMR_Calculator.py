81db2801598f82b673dbca147703b03e7057c126
Edward <happybear3000@126.com>
2019-06-07 13:31:58 +0800

    修改fractle tree,增加BMR计算

---
+++ BMR_Calculator.py
@@ -0,0 +1,56 @@
+"""
+功能：计算基础代谢率BMR
+计算公式：
+    BMR(男)=(13.7*体重(kg))+(5.0*身高(cm))-(6.8*年龄)+66
+    BMR(女)=(9.6*体重(kg))+(1.8*身高(cm))-(4.7*年龄)+655
+技术：数据类型type()
+     输入BMR参数，输出结果
+     字符串分割，格式化输出
+     异常处理
+
+"""
+def main():
+    """
+    #输入性别，体重，身高，年龄
+    gender = input('性别:')
+    #print(type(gender))
+    weight = float(input('体重(kg):'))
+    #print(type(weight))
+    height = float(input('身高(cm):'))
+    age = int(input('年龄:'))
+    """
+    print('输入参数信息，并用空格分割')
+    input_str = input('性别 体重(kg) 身高(cm) 年龄:')
+    #字符串分割
+    str_list = input_str.split(' ')
+    #异常处理
+    try:
+        gender = str_list[0]
+        weight = float(str_list[1])
+        height = float(str_list[2])
+        age = float(str_list[3])
+    
+        if gender == '男':
+            bmr = (13.7 * weight) + (5.0 * height) - (6.8 * age) + 66
+        elif gender == '女':
+            bmr = (9.6 * weight) + (1.8 * height) - (4.7 * age) + 655
+        else:
+            bmr = -1
+        
+        if bmr != -1:
+            #字符串格式化输出和占位符
+            print('性别:{},体重:{}公斤,身高:{}厘米,年龄:{}岁'.format(gender,weight,height,age))
+            print('基础代谢率BMR:{}大卡'.format(bmr))
+        else:
+            print('Invalid gender Parameter')
+    #异常处理    
+    except ValueError:
+        print('错误提示：输入信息错误！')
+    except IndexError:
+        print('错误提示：输入信息数量过少！')
+    except:
+        print('程序异常')
+        
+        
+if __name__ == '__main__':
+    main()
--- simple fractle tree.py
+++ simple fractle tree.py
@@ -2,11 +2,11 @@
 import turtle
 
 #绘制分枝
-def draw_branch(length):
+def draw_branch(length,size):
     #分形树主干颜色
     turtle.color('brown')
-    #分形树主干粗细
-    turtle.pensize(3)
+    #分形树主干粗细逐层递减
+    turtle.pensize(size - 1)
     if length > 5:
         if length <= 15:
             #分形树末端2层颜色
@@ -17,10 +17,10 @@ def draw_branch(length):
         #绘制右侧树枝
         turtle.forward(length)
         turtle.right(30)
-        draw_branch(length - 5)
+        draw_branch(length - 5, size - 1)
         #绘制左侧树枝
         turtle.left(60)
-        draw_branch(length - 5)
+        draw_branch(length - 5, size - 1)
         #返回之前的树枝
         turtle.right(30)
         turtle.penup()
@@ -34,7 +34,7 @@ def main():
     turtle.penup()
     turtle.backward(100)
     turtle.pendown()
-    draw_branch(40)
+    draw_branch(40,7)
     turtle.exitonclick()
 
 if __name__ == '__main__':
